4c8262d3f84a21487ba090301b1c3ee1d2d8a8a5
Johannes-Zi <johannes.zieres@gmail.com>
2024-06-13 16:27:18 +0200

    finished automatic computation of correlations across all clinical parameters

--- AssociationAnalysis/BasicRegression/BasicRegression.r
+++ AssociationAnalysis/BasicRegression/BasicRegression.r
@@ -1,17 +1,15 @@
+# create_new_library <- function() {
+# new_library_path <- "C:/Users/johan/Desktop/local_master_thesis_data/temp_library/"
+# dir.create(new_library_path, recursive = TRUE)
 
-create_new_library <- function() {
-new_library_path <- "C:/Users/johan/Desktop/local_master_thesis_data/temp_library/"
-dir.create(new_library_path, recursive = TRUE)
+# # Add to PATH
+# .libPaths(c(new_library_path, .libPaths()))
 
-# Add to PATH
-.libPaths(c(new_library_path, .libPaths()))
+# if (!require("BiocManager", quietly = TRUE))
+#     install.packages("BiocManager", lib = new_library_path)
 
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager", lib = new_library_path)
-
-BiocManager::install("qvalue", lib = new_library_path, force = TRUE)
-}
-
+# BiocManager::install("qvalue", lib = new_library_path, force = TRUE)
+# }
 #create_new_library()
 
 
@@ -22,7 +20,14 @@ BiocManager::install("qvalue", lib = new_library_path, force = TRUE)
 
 #BiocManager::install("qvalue")
 
+#install.packages("devtools")
+#library("devtools")
+#install_github("jdstorey/qvalue")
+
+
 library(qvalue)
+library(ggplot2)
+library(crayon)
 
 #'
 #' Import clinical metadata
@@ -133,7 +138,7 @@ create_spearman_correlations_df <- function(clinical_vector, elnet_segments_atac
   close(pb)
 
   # Add q_value column to the spearman_correlations_df
-  spearman_correlations_df$q_value <- qvalue(spearman_correlations_df$p_value)$qvalues
+  spearman_correlations_df$q_value <- qvalue(p = spearman_correlations_df$p_value)$qvalues
 
   return(spearman_correlations_df)
 }
@@ -141,12 +146,7 @@ create_spearman_correlations_df <- function(clinical_vector, elnet_segments_atac
 #'
 #' Function calculates Spearman correlation between a handed over column of the clinical metadata and the atac data
 #' represented as rows in the elnet segments atac data
-perform_spearman_correlation <- function(output_path, clinical_metadata_rna_ids, clinical_metadata_column, elnet_segments_atac_data) {
-  
-  # Extract the name of the clinical metadata column
-  passed_paramter <- deparse(substitute(clinical_metadata_column))
-  parts <- strsplit(passed_paramter, "\\$")[[1]]
-  clinical_metadata_column_name <- if(length(parts) > 1) parts[2] else parts[1]
+perform_spearman_correlation <- function(output_path, clinical_metadata_rna_ids, clinical_metadata_column, elnet_segments_atac_data, clinical_metadata_column_name) {
 
   # Create output direcotry based on the handed over output path and the clinical metadata column name
   output_dir <- paste(output_path, clinical_metadata_column_name, sep = "/")
@@ -160,8 +160,8 @@ perform_spearman_correlation <- function(output_path, clinical_metadata_rna_ids,
   # Extract clinical metadata column
   #
 
-  # Create df with the handed over column of the clinical metadata ant the RNA ids
-  clinical_metadata_column_df <- data.frame( clinical_metadata_rna_ids, clinical_metadata_column)
+  # Combine data nd create df with the handed over column of the clinical metadata and the RNA ids
+  clinical_metadata_column_df <- data.frame(clinical_metadata_rna_ids, clinical_metadata_column)
   
   # Sort rows by patient ids
   clinical_metadata_column_df <- clinical_metadata_column_df[order(clinical_metadata_column_df$clinical_metadata_rna_ids),]
@@ -174,8 +174,8 @@ perform_spearman_correlation <- function(output_path, clinical_metadata_rna_ids,
   
   # Extract the numeric vector from clinical_metadata_column_df
   # Assuming it has only one column after removing the IDs
-  clinical_vector <- as.numeric(clinical_metadata_column_df[, 1])
-
+  # Change comma to dot before making it numeric
+  clinical_vector <- as.numeric(gsub(",", ".", clinical_metadata_column_df[, 1]))
 
   #
   # Create Correlation df by looping over the elnet segments with their respective atac data 
@@ -185,14 +185,88 @@ perform_spearman_correlation <- function(output_path, clinical_metadata_rna_ids,
   #
   # Create output files
   #
+
+  message(cyan("\nCreated the following output files:\n"))
+  flush.console()
   
   # Save spearman_correlations_df as csv file with seperator ;
-  output_file_path <- paste(output_dir, paste(clinical_metadata_column_name, "spearman_correlations.csv", sep = "_"), sep = "/")
-  print(output_file_path)
+  output_file_path <- paste(output_dir, paste(clinical_metadata_column_name, "spearman_correlations.tsv", sep = "_"), sep = "/")
   write.table(spearman_correlations_df, output_file_path, row.names = FALSE, sep = ";")
-  
+  print(paste("->", output_file_path))
+
+  # Save filtered spearman_correlations_df as csv file with seperator ;
+  filtered_spearman_correlations_df <- spearman_correlations_df[spearman_correlations_df$p_value < 0.05 & spearman_correlations_df$q_value < 0.05,]
+  output_file_path <- paste(output_dir, paste(clinical_metadata_column_name, "filtered_spearman_correlations.tsv", sep = "_"), sep = "/")
+  write.table(filtered_spearman_correlations_df, output_file_path, row.names = FALSE, sep = ";")
+  print(paste("->", output_file_path))
+
+  # Create and save histogram plot of the p_values
+  p_value_hist <- ggplot(spearman_correlations_df, aes(x = p_value)) + geom_histogram(bins = 100) + ggtitle("Distribution of p_values") + xlab("p_value") + theme_light()
+  output_file_path <- paste(output_dir, paste(clinical_metadata_column_name, "p_value_hist.png", sep = "_"), sep = "/")
+  ggsave(output_file_path, p_value_hist)
+  print(paste("->", output_file_path))
+
+  # Create and save histogram plot of the q_values
+  q_value_hist <- ggplot(spearman_correlations_df, aes(x = q_value)) + geom_histogram(bins = 100) + ggtitle("Distribution of q_values") + xlab("q_value") + theme_light()
+  output_file_path <- paste(output_dir, paste(clinical_metadata_column_name, "q_value_hist.png", sep = "_"), sep = "/")
+  ggsave(output_file_path, q_value_hist)
+  print(paste("->", output_file_path))
+
+  # Create and save density plot of the spearman_correlations
+  spearman_corr_density <- ggplot(spearman_correlations_df, aes(x = spearman_corr)) + 
+    geom_density(fill = "#4dbf67",alpha = 0.4) + 
+    ggtitle("Density plot of spearman_correlations") + 
+    xlab("spearman_corr") + 
+    theme_light()
+  output_file_path <- paste(output_dir, paste(clinical_metadata_column_name, "spearman_corr_density.png", sep = "_"), sep = "/")
+  ggsave(output_file_path, spearman_corr_density)
+  print(paste("->", output_file_path))
+
+  # Create and save density plot of the spearman_correlations seperated by p_value < 0.05
+  spearman_corr_density_p_value <- ggplot(spearman_correlations_df, aes(x = spearman_corr, fill = p_value < 0.05)) +
+    geom_density(alpha = 0.5) + ggtitle("Density plot of spearman_correlations")
+  output_file_path <- paste(output_dir, paste(clinical_metadata_column_name, "spearman_corr_density_p_value.png", sep = "_"), sep = "/")
+  ggsave(output_file_path, spearman_corr_density_p_value)
+  print(paste("->", output_file_path))
+
   return(spearman_correlations_df)
 }
+
+
+iterate_over_clinical_metadata <- function(output_path, clinical_metadata, elnet_segments_atac_data){
+  
+
+  # Initialize empty list to store the spearman_correlations_df_final of each clinical metadata column
+  spearman_correlations_df_final_list <- list()
+
+  # Iterate over columns of the clinical metadata and perform spearman correlation and save output files
+  #for (i in 4:ncol(clinical_metadata)){
+  for (i in 4:ncol(clinical_metadata)){
+    # Extract current column
+    current_column <- clinical_metadata[,i]
+
+    # Perform spearman correlation
+    message(paste(cyan("\nPerform spearman correlation for:"), magenta(colnames(clinical_metadata)[i])))
+    current_spearman_correlations_df_final <- perform_spearman_correlation(output_path = output_path, clinical_metadata$rna.final.ids, current_column, elnet_segments_atac_data, clinical_metadata_column_name=colnames(clinical_metadata)[i])
+    
+    # Append current spearman_correlations_df_final to the list
+    spearman_correlations_df_final_list[[i]] <- current_spearman_correlations_df_final
+  }
+  
+  # Return list with all spearman_correlations_df_final
+  return(spearman_correlations_df_final_list)
+}
+
+#'
+#' Create combined plot with all spearman distributions
+#' 
+#' Creates a combined plot with one boxplot per clinical parameter spearman distribution
+create_combined_spearman_distributions_plot <- function(list_with_all_correlation_dfs){
+
+
+
+}
+
 
 # Import clinical metadata
 clinical_metadata_path <- "C:/Users/johan/OneDrive/dateien_cloud/Master/Semester_4/Masterarbeit/data/pulmanory_hypertension/clinical_data/version_26.05.24/isolated_metadata.csv"
@@ -217,15 +291,10 @@ reduced_clinical_metadata <- reduced_clinical_metadata(clinical_metadata, patien
 # create output_path based on cwd
 output_path <- paste(getwd(), "AssociationAnalysis/BasicRegression/combined_correlations", sep = "/")
 
+
 # Run Regression
-spearman_correlations_df_final <- perform_spearman_correlation(output_path = output_path, reduced_clinical_metadata$rna.final.ids,reduced_clinical_metadata$`mPAP`, elnet_segments_atac)
+list_with_all_correlation_dfs <- iterate_over_clinical_metadata(output_path = output_path, reduced_clinical_metadata, elnet_segments_atac)
 
-nrow(spearman_correlations_df_final)
 
-# Sort spearman_correlations_df_final by p_value
-spearman_correlations_df_final <- spearman_correlations_df_final[order(spearman_correlations_df_final$p_value),]
-head(spearman_correlations_df_final)
-# Create filtered df, which only rows with p and q value below 0.05
-filtered_spearman_correlations_df <- spearman_correlations_df_final[spearman_correlations_df_final$p_value < 0.05 & spearman_correlations_df_final$q_value < 0.05,]
-
-head(filtered_spearman_correlations_df)
+# Save R session
+#save.image("C:/Users/johan/Desktop/local_master_thesis_data/AssociationAnalysis/BasicRegression/BasicRegression.RData")
